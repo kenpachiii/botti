9acf4d1dc8df333568c0879756062c5cbfebd7e4
Michael Lewis <Kenpachi@Mac.local>
2022-04-14 02:35:07 -0400

    space

--- build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker build -t botti --build-arg GIT_TOKEN='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' --squash .
-
-
-# git clone git+https://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@github.com/kroitor/ccxt.pro.git
-
-# git+https://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@github.com/kroitor/ccxt.pro.git#subdirectory=python
