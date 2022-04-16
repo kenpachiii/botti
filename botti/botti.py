3a595d942b1e3a730913a9bac6c75c8b928c218b
Michael Lewis <Kenpachi@Mac.local>
2022-04-16 18:37:59 -0400

    reverted

--- botti/botti.py
+++ botti/botti.py
@@ -28,7 +28,7 @@ class Botti:
         self.password: str = kwargs.get('password')
         self.test: bool = kwargs.get('test')
 
-        self.symbol: str = 'BTC/USDT'
+        self.symbol: str = 'BTC/USDT:USDT'
         self.fee: float = 0.0005
         self.leverage: int = 2
 
@@ -52,13 +52,12 @@ class Botti:
         self.loop.close()
 
     # FIXME: ccxt exceptions seem to have inconsistent format
+    # also its just 'dirty'
     def log_exception(self, origin, exception) -> None:
-
-        exception = str(exception).replace(f'{self.okx.id} ', '', 1)
 
         if isinstance(exception, (ccxtpro.NetworkError, ccxtpro.ExchangeError)):
             try:
-                exception = json.loads(exception)
+                exception = json.loads(str(exception).replace(f'{self.okx.id} ', '', 1))
             except ValueError:
                 pass
 
@@ -71,6 +70,8 @@ class Botti:
                 logger.error('{id} {origin} - {code} {msg}'.format(id=self.okx.id, origin=origin, code=exception.get('data')[0].get('sCode'), msg=exception.get('data')[0].get('sMsg')))
                 send_sms('exception', 'origin: {id} {origin}\n\ncode: {code}\n\nmessage: {msg}'.format(id=self.okx.id, origin=origin, code=exception.get('data')[0].get('sCode'), msg=exception.get('data')[0].get('sMsg')))
                 return
+
+        exception = str(exception).replace(f'{self.okx.id} ', '', 1)
 
         logger.error('{id} {origin} - {error}'.format(id=self.okx.id, origin=origin, error=str(exception)))
         send_sms('exception', 'origin: {id} {origin}\n\nmessage: {msg}'.format(id=self.okx.id, origin=origin, msg=str(exception)))
@@ -145,6 +146,7 @@ class Botti:
             return False
 
         if not self.cache.last.id:
+            logger.info('{id} trailing entry - no trades found'.format(id=self.okx.id))
             return True
 
         # upper limit
@@ -234,7 +236,7 @@ class Botti:
             self.log_exception('create order', e)
 
         # reset event so consume waits on order
-        self.event.clear()
+        # self.event.clear()
 
     async def check_open_position(self):
 
@@ -243,15 +245,17 @@ class Botti:
         response: dict = {}
         try:
             response = await self.okx.fetch_position(self.symbol)
+
+            if response:
+                await self.orders_history()
+            else:
+                logger.info('{id} no position clearing cache'.format(id=self.okx.id))
+                self.cache.clear()
+
         except (ccxtpro.NetworkError, ccxtpro.ExchangeError, Exception) as e:
             self.log_exception('check open positions', e)
 
-        if response:
-            await self.orders_history()
-        else:
-            logger.info('{id} no position clearing cache'.format(id=self.okx.id))
-            self.cache.clear()
-
+    # FIXME: combine orders_history and watch_orders
     async def orders_history(self):
 
         logger.info('{id} re-syncing orders'.format(id=self.okx.id))
@@ -260,39 +264,59 @@ class Botti:
         try:
             # FIXME: params need to be 'smarter'
             # getting orders this way may not get cached by ccxtpro
-            response = await self.okx.private_get_trade_orders_history({ 'instId': self.okx.market_id(self.symbol), 'instType': 'MARGIN', 'limit': 100 })
+            response = await self.okx.private_get_trade_orders_history({ 'instId': self.okx.market_id(self.symbol), 'instType': 'SWAP', 'limit': 100 })
+
+            if response:
+                orders = self.okx.parse_orders(response.get('data'), since = self.cache.position.timestamp + 1) # add 1 so since conditional ignores most recent entry
+                
+                for order in orders:
+
+                    # TODO: does this need to be handled too...?
+                    if order.get('status') in ['canceled', 'expired', 'rejected']:
+                        logger.info('{exchange_id} recent order {status}'.format(exchange_id = self.okx.id, status = order.get('status')))
+                        continue 
+
+                    # adding position
+                    if self.cache.position.symbol == None:
+                        self.add_position(order)
+                        continue
+
+                    # updating position
+                    if self.cache.position.symbol == order.get('symbol'):
+                        self.update_position(order)
+
+                # once orders are processed re-start consuming
+                # self.event.set()
+
         except (ccxtpro.NetworkError, ccxtpro.ExchangeError, Exception) as e:
             self.log_exception('orders history', e)
 
-        if response:
-            orders = self.okx.parse_orders(response.get('data'), since = self.cache.position.timestamp + 1) # add 1 so since conditional ignores most recent entry
-            
-            for order in orders:
-
-                # TODO: does this need to be handled too...?
-                if order.get('status') in ['canceled', 'expired', 'rejected']:
-                    logger.info('{exchange_id} recent order {status}'.format(exchange_id = self.okx.id, status = order.get('status')))
-                    continue 
-
-                # adding position
-                if self.cache.position.symbol == None:
-                    self.add_position(order)
-                    continue
-
-                # updating position
-                if self.cache.position.symbol == order.get('symbol'):
-                    self.update_position(order)
-
-    # FIXME: somehow prevent double entries
     async def watch_trades(self):
+
         try:
             while True:
                 trades: list[dict] = await self.okx.watch_trades(self.symbol)
 
                 for trade in trades:
-
                     self.p_t = trade.get('price')
 
+                    # break even
+                    price, ok = self.break_even()
+                    if ok:
+                        await self.create_order('fok', 'sell', self.cache.position.open_amount, price, params = { 'tdMode': 'cross', 'posSide': 'long' })
+
+                    # trailing entry
+                    if self.trailing_entry():
+                        size = await self.position_size() 
+                        price = self.cache.last.close_avg if self.cache.last.close_avg != 0 else self.p_t
+                        await self.create_order('fok', 'buy', size, price, params = { 'tdMode': 'cross', 'posSide': 'long' })
+                        
+                    # take profits
+                    if self.take_profits():
+                        await self.create_order('fok', 'sell', self.cache.position.open_amount, self.p_t, params = { 'tdMode': 'cross', 'posSide': 'long' })
+                        logger.info('{id} take profits - target hit'.format(id=self.okx.id))
+                        send_sms('profits', 'target hit {}'.format(self.cache.last.pnl(self.leverage)))
+  
                 self.okx.trades[self.symbol].clear()
 
         except (ccxtpro.NetworkError, ccxtpro.ExchangeError, Exception) as e:
@@ -303,6 +327,7 @@ class Botti:
                 raise ccxtpro.NetworkError(e) 
 
     async def watch_order_book(self):
+
         try:
             while True:
                 self.order_book = await self.okx.watch_order_book(self.symbol, limit = 100)
@@ -314,12 +339,13 @@ class Botti:
                 raise ccxtpro.NetworkError(e) 
 
     async def watch_orders(self):
+
         try:
             while True:
 
                 # FIXME: ommiting symbol is the only way to get type ANY otherwise ccxt uses the market object
                 # which seems to produce unfavorable results
-                orders: list[dict] = await self.okx.watch_orders(limit = 1, params = { 'type': 'ANY' })
+                orders: list[dict] = await self.okx.watch_orders(self.symbol, limit = 1)
 
                 with open('dump', 'w') as json_file:
                     json.dump(self.okx.orders, json_file, 
@@ -349,7 +375,7 @@ class Botti:
                         self.update_position(order)
 
                 # once orders are processed re-start consuming
-                self.event.set()
+                # self.event.set()
 
         except (ccxtpro.NetworkError, ccxtpro.ExchangeError, Exception) as e:
             self.log_exception('watch orders', e)
@@ -360,29 +386,30 @@ class Botti:
 
     async def consume(self):
 
+        await asyncio.sleep(5)
+
         try: 
             while True:
 
                 # break even
                 price, ok = self.break_even()
                 if ok:
-                    await self.create_order('fok', 'sell', self.cache.position.open_amount, price, params = { 'tdMode': 'cross', 'ccy': 'USDT' })
+                    await self.create_order('fok', 'sell', self.cache.position.open_amount, price, params = { 'tdMode': 'cross', 'posSide': 'long' })
 
                 # trailing entry
                 if self.trailing_entry():
-                    # sz = self.okx.market(self.symbol).get('contractSize')
-                    # size = await self.position_size() 
+                    size = await self.position_size() * 0.5
                     price = self.cache.last.close_avg if self.cache.last.close_avg != 0 else self.p_t
-                    await self.create_order('fok', 'buy', 0.00001, price, params = { 'tdMode': 'cross', 'ccy': 'USDT' })
+                    await self.create_order('market', 'buy', size, None, params = { 'tdMode': 'cross', 'posSide': 'long' })
                     
                 # take profits
                 if self.take_profits():
-                    await self.create_order('market', 'sell', self.cache.position.open_amount, self.p_t, params = { 'tdMode': 'cross', 'ccy': 'USDT' })
+                    await self.create_order('fok', 'sell', self.cache.position.open_amount, self.p_t, params = { 'tdMode': 'cross', 'posSide': 'long' })
                     logger.info('{id} take profits - target hit'.format(id=self.okx.id))
                     send_sms('profits', 'target hit {}'.format(self.cache.last.pnl(self.leverage)))
 
                 # if an open or close order is created wait until watch orders receives it.
-                await self.event.wait()
+                # await self.event.wait()
 
         except (ccxtpro.NetworkError, ccxtpro.ExchangeError, Exception) as e:
             self.log_exception('consume', e)
@@ -431,8 +458,8 @@ class Botti:
             loops = [
                     self.watch_orders(),
                     self.watch_order_book(),
-                    self.watch_trades(),
-                    self.consume()
+                    self.watch_trades()
+                    # self.consume()
                 ]
 
             self.loop.run_until_complete(asyncio.gather(*loops))
