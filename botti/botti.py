bfe1556d8a9438aa41780553dc141a5d1bc896be
Michael Lewis <Kenpachi@Mac.local>
2022-04-16 20:30:43 -0400

    refactored error logs

--- botti/botti.py
+++ botti/botti.py
@@ -124,23 +124,19 @@ class Botti:
         if 'closed' not in self.cache.position.status:
             return False
 
-        if 'closed' in self.cache.position.status:
+        if not self.cache.last.id:
             logger.info('{id} trailing entry - no trades found'.format(id=self.okx.id))
             return True
 
-        # if not self.cache.last.id:
-        #     logger.info('{id} trailing entry - no trades found'.format(id=self.okx.id))
-        #     return True
+        # upper limit
+        if self.p_t > (self.cache.last.close_avg * 1.01):
+            logger.info('{id} trailing entry - no trades found - upper limit hit {limit}'.format(id=self.okx.id, limit=self.cache.last.close_avg * 1.01))
+            return True
 
-        # # upper limit
-        # if self.p_t > (self.cache.last.close_avg * 1.01):
-        #     logger.info('{id} trailing entry - no trades found - upper limit hit {limit}'.format(id=self.okx.id, limit=self.cache.last.close_avg * 1.01))
-        #     return True
-
-        # # lower limit
-        # if self.p_t < (self.cache.last.close_avg * 0.98):
-        #     logger.info('{id} trailing entry - no trades found - lower limit hit {limit}'.format(id=self.okx.id), limit=self.cache.last.close_avg * 0.98)
-        #     return True
+        # lower limit
+        if self.p_t < (self.cache.last.close_avg * 0.98):
+            logger.info('{id} trailing entry - no trades found - lower limit hit {limit}'.format(id=self.okx.id), limit=self.cache.last.close_avg * 0.98)
+            return True
 
         return False
 
